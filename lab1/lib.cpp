003b089eff79769599ae2988e240bfbb047b4d7f
Wangzhiyuan <344182022@qq.com>
2025-09-04 19:05:54 +0700

    Update lib.cpp

--- lab1/lib.cpp
+++ lab1/lib.cpp
@@ -1,12 +1,9 @@
 #include "lib.h"
 #include <iostream>
 
-// 定义版本号后缀（实际项目中可通过CI/CD自动注入仓库发布版本号）
-// 此处N对应文档中"项目的发布版本号"，需根据仓库实际发布版本调整
-const int RELEASE_VERSION = 1; 
-
+// 使用从CMake传递的宏
 int getVersion() {
-    return RELEASE_VERSION;
+    return RELEASE_N;
 }
 
 void printVersion() {
